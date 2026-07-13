1b62f9a779f7cdf0a9603746164ee4569c57b13b
genru06 <genru06@gmail.com>
2026-07-13 12:50:20 +0800

    update on pwa icons

--- quasar.config.js
+++ quasar.config.js
@@ -31,8 +31,8 @@ export default configure(function () {
           `<meta name="apple-mobile-web-app-capable" content="yes">` +
           `<meta name="apple-mobile-web-app-status-bar-style" content="default">` +
           `<meta name="apple-mobile-web-app-title" content="${appTitle}">` +
-          `<link rel="apple-touch-icon" href="${publicPath}apple-touch-icon.png">` +
-          `<link rel="apple-touch-icon" sizes="180x180" href="${publicPath}apple-touch-icon.png">` +
+          `<link rel="apple-touch-icon" href="${publicPath}icons/apple-icon-180x180.png">` +
+          `<link rel="apple-touch-icon" sizes="180x180" href="${publicPath}icons/apple-icon-180x180.png">` +
           `<link rel="apple-touch-icon" sizes="167x167" href="${publicPath}icons/apple-icon-167x167.png">` +
           `<link rel="apple-touch-icon" sizes="152x152" href="${publicPath}icons/apple-icon-152x152.png">` +
           `<link rel="apple-touch-icon" sizes="120x120" href="${publicPath}icons/apple-icon-120x120.png">` +
